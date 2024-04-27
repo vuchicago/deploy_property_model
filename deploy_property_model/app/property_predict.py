f90cc7597ae8abcd8aa87b359de6ac7b6bf11de5
Vu Nguyen <vuchicago@gmail.com>
2024-04-26 22:59:19 -0500

    fixed requirements.txt and fastapi

--- deploy_property_model/app/property_predict.py
+++ deploy_property_model/app/property_predict.py
@@ -6,8 +6,10 @@ from typing import Any
 os.chdir(r'/Users/vuchicago/Python/deploy_property_model/model_pipeline/data/train')
 
 #%%
-with open("vectorizer_prop.pickle", "rb") as f:
-    vectorizer_prop = pickle.load(f)
+with open("vectorizer_prop_credit.pickle", "rb") as f:
+    vectorizer_prop_credit = pickle.load(f)
+with open("vectorizer_prop_debit.pickle", "rb") as f:
+    vectorizer_prop_debit = pickle.load(f)
 
 with open("property_pred_debit.pickle", "rb") as f:
     model_property_debit = pickle.load(f)
@@ -29,18 +31,30 @@ class PropertyPredict(BaseModel):
         def predict(self,text_input:TextInput):
                 input=self.vectorizer.transform([text_input.text_input])
                 output=self.model.predict(input)
-                return output
+                output_prob=max(self.model.predict_proba(input))
+                return output,output_prob
 
-def prediction(Description):
-        predictor = PropertyPredict(vectorizer=vectorizer_prop, model=model_property_credit)
+
+
+def prediction_prop_debit(Description):
+        predictor = PropertyPredict(vectorizer=vectorizer_prop_debit, model=model_property_debit)
         input_data = TextInput(text_input=Description)
-        result = predictor.predict(input_data)
-        return str(result)
-        
+        result,output_prob = predictor.predict(input_data)
+        return result, output_prob
+
+def prediction_prop_credit(Description):
+        predictor = PropertyPredict(vectorizer=vectorizer_prop_credit, model=model_property_credit)
+        input_data = TextInput(text_input=Description)
+        result,output_prob = predictor.predict(input_data)
+        return result,output_prob
+
+
 
 
         
         
 
 
+# %%
+prediction_prop_credit('Laramie')
 # %%
